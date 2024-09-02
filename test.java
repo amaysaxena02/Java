357af5387bd8fb599aebc68998b3d9035830bd60
Amay Saxena <amaysaxena8@gmail.com>
2024-09-02 23:04:40 +0530

    Initial commit

---
+++ test.java
@@ -0,0 +1,31 @@
+public class test {
+    public static void main(String args[])
+    {
+        System.out.println("hello");
+
+        String name = "Ashu";
+        System.out.println(name);
+
+        int a = 10;
+        int b = 15;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(a+b);
+
+        float c =  14.67f, d = 17.19f;
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(c+d);
+
+        if(c+d==31.86f)
+        {
+            boolean myBool = true;
+            System.out.println(myBool);
+        }
+        else
+        {
+            boolean myBool = false;
+            System.out.println(myBool);
+        }  
+    }
+}

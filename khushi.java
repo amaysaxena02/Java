8b73c5861c1f8e3062255c63703f042999cdc5e7
Amay Saxena <amaysaxena8@gmail.com>
2024-09-03 14:56:48 +0530

    scan fix

--- khushi.java
+++ khushi.java
@@ -1,14 +1,20 @@
 import java.util.Scanner;
-public class khushi {
-    public static void main(String[] args)
-    {
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Enetr value of a: ");
-        int a = scan.nextInt();
-        System.out.println("Enetr value of b: ");
-        int b = scan.nextInt();
 
-        System.out.println("\nSum of a and b: "+ (a+b));
+public class khushi {
+    public static void main(String[] args) {
+        Scanner scan = null;
+        try {
+            scan = new Scanner(System.in);
+            System.out.println("Enter value of a: ");
+            int a = scan.nextInt();
+            System.out.println("Enter value of b: ");
+            int b = scan.nextInt();
+
+            System.out.println("\nSum of a and b: " + (a + b));
+        } finally {
+            if (scan != null) {
+                scan.close(); // Ensure the Scanner is closed
+            }
+        }
     }
-    
 }

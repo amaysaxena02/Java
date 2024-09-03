35cbce9419d19921955559cb3ec5cc696f15b191
Amay Saxena <amaysaxena8@gmail.com>
2024-09-03 14:53:03 +0530

    minor changes

---
+++ khushi.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class khushi {
+    public static void main(String[] args)
+    {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enetr value of a: ");
+        int a = scan.nextInt();
+        System.out.println("Enetr value of b: ");
+        int b = scan.nextInt();
+
+        System.out.println("\nSum of a and b: "+ (a+b));
+    }
+    
+}

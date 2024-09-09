559ed6fe738dda1b09e3cc36ec0c539e52fb4edc
Amay Saxena <amaysaxena8@gmail.com>
2024-09-09 19:44:10 +0530

    minor changes

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Java
 
-In this repository, I will commit all the programs which I have created while learning java to keep a track
+In this repository, I will commit all the programs which I have created while learning java to keep a track.
+I am also learnig DSA with java.
---
+++ bool.java
@@ -0,0 +1,14 @@
+public class bool {
+    public static void main(String[] args)
+    {
+        boolean isJavafun = true;
+        boolean isFishTasty = false;
+        System.out.println(isJavafun);
+        System.out.println(isFishTasty  );
+
+        int x = 10, y = 20;
+        System.out.println(x<y);
+        System.out.println(x==10);
+        System.out.println(x==y);
+    }
+}

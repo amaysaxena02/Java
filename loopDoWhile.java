5cad0aad65df15aae82b8ecb7f4648b41faa7bb2
Amay Saxena <amaysaxena8@gmail.com>
2024-09-10 22:30:25 +0530

    dowhile

---
+++ loopDoWhile.java
@@ -0,0 +1,19 @@
+public class loopDoWhile {
+    public static void main(String[] args) {
+        int i = 0;
+        do{
+            System.out.println("hello\n");
+            i++;
+        }
+        while(i<5);
+
+
+        int x = 3;
+        do{
+            System.out.println(x);
+            x--;
+        }
+        while(x>=0);
+        System.out.println("\nHappy New Year");
+    }
+}
---
+++ loopWhile.java
@@ -0,0 +1,14 @@
+public class loopWhile {
+    public static void main(String[] args) {
+        int i = 0;
+        while(i<=10){
+            System.out.println(i);
+            i++;
+        }
+        int y = 0;
+        while(y < 10){
+            System.out.println("\nHiiiiiiiiiii");
+            y++;
+        }
+    }
+}
